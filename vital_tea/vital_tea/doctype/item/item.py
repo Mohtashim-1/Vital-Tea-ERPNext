a09b84aef3019aa82f3a8fe273f93f354f0a53aa
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-07-30 21:48:26 +0000

    feat: add fields added

--- vital_tea/vital_tea/doctype/item/item.py
+++ vital_tea/vital_tea/doctype/item/item.py
@@ -7,27 +7,27 @@ from frappe.model.document import Document
 
 class Item(Document):
 	def before_save(self):
-        self.package_detail()
+            self.package_detail()
+        
+        def package_detail(self):
+            self.custom_packaging_detail = str(self.weight_per_unit) +" X "+ str(self.custom_item_packing) 
+            # doc.custom_packaging_detail = str(doc.weight_per_unit) +" X "+ str(doc.custom_item_packing)
+            if self.weight_uom == "Gram":
+                self.custom_weight_net_kgs =  (self.weight_per_unit * self.custom_item_packing) / 1000
+                # frappe.msgprint("test")
+            else:
+                self.custom_weight_net_kgs =  (self.weight_per_unit * self.custom_item_packing)
+                
+            # custom_weight_gross_kgs
 
-    def package_detail(self):
-        self.custom_packaging_detail = str(self.weight_per_unit) +" X "+ str(self.custom_item_packing) 
-        # doc.custom_packaging_detail = str(doc.weight_per_unit) +" X "+ str(doc.custom_item_packing)
-        if self.weight_uom == "Gram":
-            self.custom_weight_net_kgs =  (self.weight_per_unit * self.custom_item_packing) / 1000
-            # frappe.msgprint("test")
-        else:
-            self.custom_weight_net_kgs =  (self.weight_per_unit * self.custom_item_packing)
-            
-        # custom_weight_gross_kgs
+            # self.custom_weight_gross_kgs = self.custom_weight_net_kgs + (1.6)
+                
+            self.custom_weight_gross_kgs = self.custom_weight_net_kgs + self.custom_packaging_weight_kgs
 
-        # self.custom_weight_gross_kgs = self.custom_weight_net_kgs + (1.6)
-            
-        self.custom_weight_gross_kgs = self.custom_weight_net_kgs + self.custom_packaging_weight_kgs
+            # lbs
 
-        # lbs
+            self.custom_lbs = self.weight_per_unit * 0.00220462
 
-        self.custom_lbs = self.weight_per_unit * 0.00220462
+            # oz
 
-        # oz
-
-        self.custom_oz = self.weight_per_unit * 0.035274
+            self.custom_oz = self.weight_per_unit * 0.035274            
